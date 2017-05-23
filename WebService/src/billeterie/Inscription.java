e47322eb5fa329fc8ed9c32f9d05190a2aea9fdf
damien <dami13795@msn.com>
2017-05-23 10:30:03 +0200

    WS : Import de Bootstrap

--- WebService/src/billeterie/Inscription.java
+++ WebService/src/billeterie/Inscription.java
@@ -17,14 +17,15 @@ public class Inscription extends HttpServlet{
 	 */
 	private static final long serialVersionUID = 1L;
 	public static final String vue="/WEB-INF/index.jsp";
-			
+	Collection<User> set = new HashSet<User>();
+	
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 		this.getServletContext().getRequestDispatcher( vue ).forward(request, response);
 	}
 	
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 		InscriptionForm form = new InscriptionForm();
-		Collection<User> set = new HashSet<User>();
+		
 		form.inscrire(request);
 		set.addAll(form.getListUser());
 		System.out.println(set);
