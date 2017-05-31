2c1931ebb31e1daabc9b0d729202a136ed6a5959
jcivel <jcivel@stella.emi.u-bordeaux.fr>
2017-05-31 15:49:02 +0200

    Salut saluuuuut, moi c'est Jujuuuuu, faites attentioooon, je vais vous faireeeu du rap

--- WebService/src/billeterie/Services.java
+++ WebService/src/billeterie/Services.java
@@ -1,11 +1,51 @@
 package billeterie;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Services {
 
 	
-	  public int additionner(int valeur1, int valeur2) {
-          return valeur1 + valeur2;
-    }
-	  
-	  
+	public boolean addEvent(Evenement event)
+	{
+		return true;
+	}
+	
+	public boolean deleteEvent(int idEventASuppr)
+	{
+		return true;
+	}
+	
+	public boolean modifyEvent(Evenement newEvent, int idEventAModifier)
+	{
+		return true;
+	}
+	
+	public boolean doReservation(Reservation reservation, Evenement event, User user)
+	{
+		return true;
+	}
+	
+	public List<Evenement> getAllEvents()
+	{
+		
+		List<Evenement> liste = new ArrayList<Evenement>();
+		return liste;
+	}
+	
+	public Evenement getEvent(int idEvent)
+	{
+		
+		Evenement event = new Evenement();
+		return event ;
+	}
+	
+	public boolean ajouterEvent(Evenement event)
+	{
+		return true;
+	}
+	
+	
+	
+	
 }
