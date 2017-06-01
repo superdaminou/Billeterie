8f8b7d4a75780773e0899d5f339e8b8f03b3b135
damien <dami13795@msn.com>
2017-06-01 17:16:19 +0200

    Suppresion Inscription -> Acceuil page default

--- WebService/src/billeterie/Acceuil.java
+++ WebService/src/billeterie/Acceuil.java
@@ -1,8 +1,11 @@
 package billeterie;
 
+import jaxb.*;
+
 import java.io.IOException;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 
 import javax.jws.WebService;
 import javax.servlet.ServletException;
@@ -18,27 +21,42 @@ public class Acceuil extends HttpServlet{
 	
 	private static final long serialVersionUID = 1L;
 	public static final String vue="/WEB-INF/index.jsp";
-	Collection<User> set = new HashSet<User>();
+	public static final String vueCrea="/WEB-INF/ajoutEvent.jsp";
+	Collection<Evenement> set = new HashSet<Evenement>();
 	
 	/* (non-Javadoc)
 	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
 	 */
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		ServiceEvenement service=new ServiceEvenement();
+		List<Evenement> listEvent= service.getAllEvents();
+		set.addAll(listEvent);
 		this.getServletContext().getRequestDispatcher( vue ).forward(request, response);
+		
 	}
 	
 	/* (non-Javadoc)
 	 * @see javax.servlet.http.HttpServlet#doPost(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
 	 */
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-		InscriptionForm form = new InscriptionForm();
-		
-		form.inscrire(request);
-		set.addAll(form.getListUser());
-		System.out.println(set.toString());
-		
-		request.setAttribute("set", set);
-		this.getServletContext().getRequestDispatcher(vue).forward(request, response);
+		//On regarde si l'utilisateur a cliqué sur le bouton Réservation
+				if(request.getParameter("btnResa")!= null)
+				{
+					
+				InscriptionForm form = new InscriptionForm();
+				form.inscrire(request);
+				System.out.println(set.toString());
+				
+				request.setAttribute("set", set);
+				this.getServletContext().getRequestDispatcher(vue).forward(request, response);
+				}
+				
+				
+				//Si non, alors il a cliqué sur Création, on le redirige.
+				else
+				{
+					this.getServletContext().getRequestDispatcher(vueCrea).forward(request, response);
+				}
 	}
 
 }
